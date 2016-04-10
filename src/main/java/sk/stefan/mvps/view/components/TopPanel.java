de9abb84fba019fb808951265f74384b03531f18
elopin <elopin@seznam.cz>
2016-04-10 22:33:48 +0200

    lokalizace demo

--- src/main/java/sk/stefan/mvps/view/components/TopPanel.java
+++ src/main/java/sk/stefan/mvps/view/components/TopPanel.java
@@ -1,22 +1,35 @@
 package sk.stefan.mvps.view.components;
 
 import com.vaadin.annotations.DesignRoot;
+import com.vaadin.client.ui.VTabsheet;
 import com.vaadin.event.ShortcutAction;
+import com.vaadin.server.ThemeResource;
+import com.vaadin.server.VaadinSession;
 import com.vaadin.spring.annotation.SpringComponent;
 import com.vaadin.spring.annotation.VaadinSessionScope;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.HasComponents;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
 import com.vaadin.ui.declarative.Design;
 import org.springframework.beans.factory.annotation.Autowired;
 import sk.stefan.mvps.model.entity.A_User;
 import sk.stefan.mvps.model.service.SecurityService;
 import sk.stefan.mvps.model.service.UserService;
+import sk.stefan.mvps.view.MainTabsheet;
+import sk.stefan.mvps.view.tabs.TabComponent;
+import sk.stefan.ui.NavigationMenu;
 import sk.stefan.utils.Localizator;
 import sk.stefan.utils.VaadinUtils;
+
+import java.util.LinkedList;
+import java.util.Locale;
+import java.util.function.Consumer;
 
 /**
  * Horní panel s přihlášením uživatele.
@@ -36,6 +49,7 @@ public class TopPanel extends HorizontalLayout {
 
     private Label lblCaption;
     private HorizontalLayout basic;
+    private Button butLang;
     private Label lblUser;
     private Button butLogin;
     private Button butLogout;
@@ -56,6 +70,53 @@ public class TopPanel extends HorizontalLayout {
         butSend.addClickListener(event -> onSend());
         butSend.setClickShortcut(ShortcutAction.KeyCode.ENTER);
 
+        butLang.setIcon(new ThemeResource("images/flags/united_states.png"));
+        butLang.addClickListener(e -> changeLocale());
+
+    }
+
+    private void changeLocale() {
+        Locale locale = VaadinSession.getCurrent().getLocale();
+        if (locale.equals(Locale.US)) {
+            VaadinSession.getCurrent().setLocale(VaadinUtils.DEFAULT_LOCALE);
+            butLang.setIcon(new ThemeResource("images/flags/united_states.png"));
+        } else {
+            VaadinSession.getCurrent().setLocale(VaadinUtils.US_LOCALE);
+            butLang.setIcon(new ThemeResource("images/flags/slovakia.png"));
+        }
+        walkComponentTree(UI.getCurrent(), (component) -> {
+            if (MainTabsheet.class.isAssignableFrom(component.getClass())) {
+                MainTabsheet tabsheet = (MainTabsheet) component;
+                LinkedList<Component> tabs = new LinkedList<>();
+                tabsheet.iterator().forEachRemaining(tab -> {
+                    tabs.add(tab);
+                });
+                tabsheet.removeAllComponents();
+
+                tabs.forEach(tab -> {
+                    String caption = Localizator.getLocalizedMessage(tab.getClass().getCanonicalName(), ".cap", null, VaadinUtils.getLocale());
+                    if (caption == null) {
+                        caption = ((TabComponent) tab).getTabCaption();
+                    }
+                    tab.setCaption(caption);
+                    tabsheet.addTab(tab, caption).setClosable(true);
+                });
+            } else if (component.getClass().getAnnotation(DesignRoot.class) != null){
+                Localizator.localizeDesign(component);
+            } else if (NavigationMenu.class.isAssignableFrom(component.getClass())) {
+                NavigationMenu navigationMenu = (NavigationMenu) component;
+                navigationMenu.updateLocalization();
+            }
+        });
+    }
+
+    private void walkComponentTree(Component component, Consumer<Component> consumer) {
+        consumer.accept(component);
+        if (component instanceof HasComponents) {
+            for (Component child : ((HasComponents)component)) {
+                walkComponentTree(child, consumer);
+            }
+        }
     }
 
     public void setLoginListener(LoginListener loginListener) {
